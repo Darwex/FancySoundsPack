d9edce63b67f223a24d8bb177cb8425faaf72824
Vrsek, David <david.vrsek@merck.com>
2017-05-28 14:28:39 +0200

    Repo init

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+  Component,
+  StyleSheet
+} from 'react-native'
+
+App extends Component {
+
+  render() {
+    return (
+      <View>
+      </View>
+    )
+  }
+}
+
+Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
